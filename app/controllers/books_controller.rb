4f6de4982be1d76730c2fe3bf73dc043c7f98eff
wwwnagumo <www.03140624@outlook.jp>
2020-02-01 12:21:35 +0900

    [Add]5-9コントローラのアクション/アソシエーションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,40 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.find(book_params)
+    book.save
+    redirect_to book_path
   end
 
   def show
+    book = User.find(params[:id])
   end
 
   def edit
+    book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :profile_image)
   end
 end
