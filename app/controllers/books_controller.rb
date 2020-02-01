e3ee7adf7c10a5c3451f550aa846894d41376cc9
wwwnagumo <www.03140624@outlook.jp>
2020-02-01 19:15:26 +0900

    [Add]5-9バリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,13 +15,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    if book.save
       flash[:notice] = "You have creatad book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(book.id)
     else
-      render :new
+      render action: :index
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   attachment :profile_image
+  validates :introduction, length: { in: 1..50 }
 end
