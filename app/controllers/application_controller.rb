3c2b5f8f9cceee5bb714c75454bfb7349790c903
wwwnagumo <www.03140624@outlook.jp>
2020-02-01 12:01:18 +0900

    [Add]5-9top/aboutレイアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :authenticate_user!, only: [:show, :index]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'books#top'
   get '/home/about' => 'books#home'
   resources :books
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
